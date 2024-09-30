e446f16d6bbfa704dcf992bc61351ac2a87bcf5e
oberrow <72581470+oberrow@users.noreply.github.com>
2024-09-30 11:55:42 -0400

    Update fireworks.c

--- src/fireworks.c
+++ src/fireworks.c
@@ -251,31 +251,3 @@ uintptr_t random_seed()
         seed = time(NULL);
     return seed;
 }
-asm (
-    "\
-    .intel_syntax noprefix;\
-    .global random_seed_x86_64;\
-    random_seed_x86_64:;\
-    push rbp; mov rbp, rsp;\
-    .rdrand:;\
-	    mov eax, 1;\
-	    xor ecx,ecx;\
-	    cpuid;\
-	    bt ecx, 30;\
-	    jnc .rdseed;\
-	    rdrand rax;\
-	    jnc .rdrand;\
-	    jmp .done;\
-    .rdseed:;\
-    	mov eax, 7;\
-    	xor ecx,ecx;\
-    	cpuid;\
-    	bt ebx, 18;\
-    	jnc .done;\
-    	rdseed rax;\
-    	jnc .rdseed;\
-    .done:;\
-    leave; ret;\
-    .att_syntax prefix;\
-    "
-);
